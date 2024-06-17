a7ccd37612fea8665e62655200a2b7f259cf76e5
Manjit <123manjittoppo@gmail.com>
2024-06-17 22:23:01 +0530

    commit to mongodb atlas

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+
+const asyncHandler= (requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).asyncHandler.
+        catch((err)=>next(err))
+    }
+}
+
+//Above code based on promise 
+
+export{asyncHandler}
+
+
+
+
+
+/*
+const asyncHandler =(fn) =>async(req,res,next)=>{
+ try{
+ await fn(req,res,next)
+ }catch(error){
+ res.status(err.code||500).json({
+ success:false,
+ message:err.message })}  
+}
+Above code based on try catch method
+*/ 
